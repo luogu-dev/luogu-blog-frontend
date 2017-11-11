bb3b900e98952e91c0d2893d00b3ee5841c6f9f9
Toto Lin <csl@lunlimited.net>
2017-11-11 10:59:12 +0900

    Fix mdblog templates

--- src/templates/mdblog/layouts/vote_count.vue
+++ src/templates/mdblog/layouts/vote_count.vue
@@ -1,12 +1,12 @@
 <template>
       <div id = "vote-count" class="mdui-float-right">
           <i class="mdui-icon material-icons"  :class="{ 'mdui-text-color-theme': (vote == 0), 'mdui-text-color-theme-accent': (vote !== 0) }">thumb_up</i>
-          <i class="mdui-typo-caption-opacity" :class="{ 'mdui-text-color-theme': (vote == 0), 'mdui-text-color-theme-accent': (vote !== 0}) }">{{ thumbUp }}</i>
+          <i class="mdui-typo-caption-opacity" :class="{ 'mdui-text-color-theme': (vote == 0), 'mdui-text-color-theme-accent': (vote !== 0) }">{{ thumbUp }}</i>
       </div>
 </template>
 
 <script>
-  import defaultData from 'scripts/article_vote'
+  import { defaultData } from 'scripts/article_vote'
   export default {
     data: defaultData,
     computed: {},
