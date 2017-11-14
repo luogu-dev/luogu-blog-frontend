8263959bc297f800e996fa50d7a6965f25dbf7b0
Toto Lin <csl@lunlimited.net>
2017-11-14 22:23:43 +0900

    HolaKit theme

---
+++ src/templates/holakit/layouts/article_list.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="article-list" class="hola-container hola-columns hola-card-stack">
+    <div class="hola-columns-item" v-for="post in posts">
+      <a :href="post.Identifier" style="text-decoration: none; color: var(--hola-text-dark-color);">
+        <div class="hola-card">
+          <h2 class="hola-card-title">
+            {{ post.Title }}
+          </h2>
+          <p>{{ post.ContentDescription }}</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { defaultData, defaultMounted, getPosts } from 'scripts/article_list'
+  import formatDate from 'plugins/format_date'
+  export default {
+    data: defaultData,
+    mounted: defaultMounted,
+    methods: { getPosts },
+    filters: { formatDate },
+    components: { }
+  }
+</script>
