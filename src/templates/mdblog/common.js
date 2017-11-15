164b23c0bfc6fbb1679e62f536b55471908434b8
dark-flames <dark_flames@outlook.com>
2017-11-16 01:41:34 +0800

    Update mdblog

---
+++ src/templates/mdblog/common.js
@@ -0,0 +1,26 @@
+import 'mdui.css'
+import 'mdui'
+import './styles/style.css'
+import $ from 'jquery'
+
+var scrollDelay;
+function pageScroll () {
+  window.scrollBy(0, -50)
+  scrollDelay = setTimeout(pageScroll, 10)
+  if ($(window).scrollTop() <= 10)
+    clearTimeout(scrollDelay)
+}
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 10) {
+    $('#topbar').addClass('mdui-color-theme')
+    $('#small-header').removeClass('mdui-shadow-0')
+  } else {
+    $('#topbar').removeClass('mdui-color-theme')
+    $('#small-header').addClass('mdui-shadow-0')
+  }
+})
+
+$(document).ready(function () {
+  $('#mdblog-scroll').click(pageScroll)
+})
