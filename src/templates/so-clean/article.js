40b738895ae758fec4f4555e4e379de5401840eb
Soha Jin <soha@lohu.info>
2017-11-17 15:46:17 +0800

    add vote and comment to article page

--- src/templates/so-clean/article.js
+++ src/templates/so-clean/article.js
@@ -1,5 +1,21 @@
 import Vue from 'vue'
 import './common'
+import { initKatex } from 'scripts/article'
+import ArticleComments from './layouts/article_comments.vue'
+import ArticleVote from './layouts/article_vote.vue'
+import 'components/comment.css'
+import 'components/button.css'
+import 'components/form.css'
+import 'components/icon.css'
 
-window.articleVote = new Vue({})
-window.articleComments = new Vue({})
+initKatex()
+
+window.articleVote = new Vue({
+  el: '#article-vote',
+  render: h => h(ArticleVote)
+})
+
+window.articleComments = new Vue({
+  el: '#article-comments',
+  render: h => h(ArticleComments)
+})
---
+++ src/templates/so-clean/layouts/article_vote.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="article-vote">
+		<div v-if="uid">
+			<div v-if="vote===0">
+				<a @click="performVote(1)">赞 <span>{{ thumbUp }}</span></a> | <a @click="performVote(-1)">踩</a>
+			</div>
+			<div v-else>
+				<a @click="performVote(vote)">取消<span v-if="vote==1">赞</span><span v-if="vote==-1">踩</span> <span>{{ thumbUp }}</span></a>
+			</div>
+		</div>
+		<div v-else>
+			本文目前已有 {{ thumbUp }} 个赞。
+		</div>
+  </div>
+</template>
+
+<script>
+  import { defaultData, performVote } from 'scripts/article'
+  export default {
+    data: defaultData,
+    computed: {
+      canVoteUp () {
+        return this.checkCanVote(1)
+      },
+      canVoteDown () {
+        return this.checkCanVote(-1)
+      }
+    },
+    methods: {
+      performVote,
+      checkCanVote (voteType) {
+        // Must be logged in, and either not voted,
+        // or voted the same voteType,
+        // (in which case triggers un-vote)
+        return this.uid && (this.vote === 0 || this.vote === voteType)
+      }
+    }
+  }
+</script>
