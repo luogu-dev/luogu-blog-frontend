b18918cbdb992e0cc1a650d5ab1502a0e2333abb
Soha Jin <soha@lohu.info>
2017-11-19 13:26:41 +0800

    fix code-style, add a link to the blog admin panel Add LICENSE of the original theme

--- src/templates/so-clean/components/pagination.vue
+++ src/templates/so-clean/components/pagination.vue
@@ -1,8 +1,8 @@
 <template>
 	<nav class="pagination" role="navigation">
-		<a class="newer-posts" @click="callback(page - 1)" v-if="page>1"><span aria-hidden="true">&larr;</span> 新点儿的</a>
+		<a class="newer-posts" @click="callback(page - 1)" v-if="page > 1"><span aria-hidden="true">&larr;</span> 新点儿的</a>
 		<span class="page-number">第 {{ page }} / {{ totalPages }} 页</span>
-		<a class="older-posts" @click="callback(page + 1)" v-if="page<totalPages">旧点儿的 <span aria-hidden="true">&rarr;</span></a>
+		<a class="older-posts" @click="callback(page + 1)" v-if="page < totalPages">旧点儿的 <span aria-hidden="true">&rarr;</span></a>
 	</nav>
 </template>
 
