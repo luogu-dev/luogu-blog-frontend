006b407a9b00ecfeb7de90e08d7aa8992a997a35
Toto Lin <toto@linux.com>
2017-11-23 01:11:18 +0900

    Fix code highlighting

--- src/templates/debug/components/pagination.vue
+++ src/templates/debug/components/pagination.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="ui three column centered grid" style="margin: 30px;">
     <div class="ui pagination menu" v-if="totalPages > 1">
-      <a @click="callback(1)" :class="{ disabled: page == 1 }" class="item">&lt;&lt;</a>
-      <a @click="callback(page - 1)" :class="{ disabled: page == 1 }" class="item">&lt;</a>
+      <a @click="callback(1)" :class="{ disabled: page == 1 }" class="item"><i class="chevron circle left icon"></i></a>
+      <a @click="callback(page - 1)" :class="{ disabled: page == 1 }" class="item"><i class="chevron left icon"></i></a>
       <a @click="callback(page - 2)" v-if="page - 2 >= 1" class="item">{{ page - 2 }}</a>
       <a @click="callback(page - 1)" v-if="page - 1 >= 1" class="item">{{ page - 1 }}</a>
       <a class="active item">{{ page }}</a>
       <a @click="callback(page + 1)" v-if="page + 1 <= totalPages" class="item">{{ page + 1 }}</a>
       <a @click="callback(page + 2)" v-if="page + 2 <= totalPages" class="item">{{ page + 2 }}</a>
-      <a @click="callback(page + 1)" :class="{ disabled: page == totalPages }" class="item">&gt;</a>
-      <a @click="callback(totalPages)" :class="{ disabled: page == totalPages }" class="item">&gt;&gt;</a>
+      <a @click="callback(page + 1)" :class="{ disabled: page == totalPages }" class="item"><i class="chevron right icon"></i></a>
+      <a @click="callback(totalPages)" :class="{ disabled: page == totalPages }" class="item"><i class="chevron circle right icon"></i></a>
     </div>
   </div>
 </template>
--- src/templates/so-clean/article.js
+++ src/templates/so-clean/article.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import './common'
-import { initKatex } from 'scripts/article'
+import { initKatex, initHighlight } from 'scripts/article'
 import ArticleComments from './layouts/article_comments.vue'
 import ArticleVote from './layouts/article_vote.vue'
 import 'components/comment.css'
 import 'components/button.css'
 import 'components/form.css'
 import 'components/icon.css'
+import 'highlight.js/styles/tomorrow.css'
 
 initKatex()
+initHighlight()
 
 window.articleVote = new Vue({
   el: '#article-vote',
