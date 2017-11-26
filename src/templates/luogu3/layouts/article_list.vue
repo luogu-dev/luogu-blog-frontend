7e85c4b398ae35b0056dbda7d32f6a082389208b
kkksc03 <xmwcq@qq.com>
2017-11-26 08:08:21 +0800

    luogu3 theme

---
+++ src/templates/luogu3/layouts/article_list.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="article-list">
+		
+		<div v-if="ready">
+			<article v-for="post in posts" class="lg-article am-g">
+			  <h3 class="am-article-title blog-title">
+				<a :href="post.Identifier">{{ post.Title }}</a>
+			  </h3>
+			  <h4 class="am-article-meta blog-meta"> 
+				<span class="am-icon-thumbs-up"> {{ post.ThumbUp }}</span>
+				|
+				posted on {{ post.PostTime | formatDate }} | under <a href="#">分类</a>
+			    
+			  </h4>
+			  <p>{{ post.ContentDescription }}</p>
+			</article>
+		</div>
+		<loader v-else></loader>
+
+		<pagination v-if="ready" :page="page" :totalPages="totalPages" :callback="getPosts"></pagination>
+    </div>
+</template>
+
+<script>
+  import pagination from '../components/pagination.vue'
+  import loader from '../components/loader.vue'
+  import { defaultData, defaultMounted, getPosts } from 'scripts/article_list'
+  import formatDate from 'plugins/format_date'
+  export default {
+    data: defaultData,
+    mounted: defaultMounted,
+    methods: { getPosts },
+    filters: { formatDate },
+    components: { pagination, loader }
+  }
+</script>
---
+++ src/templates/luogu3/layouts/article_vote.vue
@@ -0,0 +1,31 @@
+<template>
+  <span id="article-vote" class="am-comment-actions">
+	  <a href="#" @click="performVote(1)"><i class="am-icon-thumbs-up" :class="{ voteactive: vote === 1}"></i></a> {{ thumbUp }}
+	  <a href="#" @click="performVote(-1)"><i class="am-icon-thumbs-down" :class="{ disabled: !canVoteDown, voteactive: vote === -1 }"></i></a> 
+	  <a href="#"><i class="am-icon-reply"></i></a>
+  </span>
+</template>
+
+<script>
+  import { defaultData, performVote } from 'scripts/article'
+  export default {
+    data: defaultData,
+    computed: {
+      canVoteUp () {
+        return this.checkCanVote(1)
+      },
+      canVoteDown () {
+        return this.checkCanVote(-1)
+      }
+    },
+    methods: {
+      performVote,
+      checkCanVote (voteType) {
+        // Must be logged in, and either not voted,
+        // or voted the same voteType,
+        // (in which case triggers un-vote)
+        return this.uid && (this.vote === 0 || this.vote === voteType)
+      }
+    }
+  }
+</script>
