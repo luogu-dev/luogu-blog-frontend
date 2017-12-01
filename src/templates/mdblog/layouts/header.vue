54496d55d8c6ea4ac079a5d12c6bca75936fc73b
dark-flames <dark_flames@outlook.com>
2017-12-02 01:48:33 +0800

    Reformat

--- src/templates/mdblog/layouts/header.vue
+++ src/templates/mdblog/layouts/header.vue
@@ -2,8 +2,9 @@
   <div id="small-header" class="mdui-appbar mdui-shadow-0"
        style="position: fixed;z-index: 100;top:0;">
     <div id="topbar" class="mdui-toolbar ">
-      <a id="md-menu" class="mdui-btn mdui-btn-icon"><i
-        class="mdui-icon material-icons" mdui-drawer="{target: '#mdblog-drawer', swipe: true, overlay: true}">menu</i></a>
+      <a id="md-menu" class="mdui-btn mdui-btn-icon">
+        <i class="mdui-icon material-icons" mdui-drawer="{target: '#mdblog-drawer', swipe: true, overlay: true}">menu</i>
+      </a>
       <div class="mdui-toolbar-spacer"></div>
       <button class="mdui-btn mdui-btn-icon mdui-ripple" @click="getPosts(1)">
         <i class="mdui-icon material-icons">search</i>
