d0f28941c777a930770470397686d62137c87abe
dark-flames <dark_flames@outlook.com>
2017-12-12 01:46:40 +0800

    Update mdblob to support config

--- src/templates/mdblog/layouts/header.vue
+++ src/templates/mdblog/layouts/header.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="small-header" class="mdui-appbar mdui-shadow-0"
-       style="position: fixed;z-index: 100;top:0;">
+       style="position: fixed;z-index: 100;top:0;background-color:transparent;">
     <div id="topbar" class="mdui-toolbar ">
       <a id="md-menu" class="mdui-btn mdui-btn-icon">
         <i class="mdui-icon material-icons" mdui-drawer="{target: '#mdblog-drawer', swipe: true, overlay: true}">menu</i>
       </a>
       <div class="mdui-toolbar-spacer"></div>
-      <button class="mdui-btn mdui-btn-icon mdui-ripple" @click.prevent="search()">
+      <button class="mdui-btn mdui-btn-icon mdui-ripple" @click.prevent="search()" style="margin-right: 0;">
         <i class="mdui-icon material-icons">search</i>
       </button>
-      <input style="max-width: 20%;" class="mdui-textfield-input" type="text" placeholder="Search" v-model="keyword"/>
+      <div class="mdui-textfield" style="margin-left: 0;">
+      <input class="mdui-textfield-input" type="text" placeholder="Search" v-model="keyword"/>
+      </div>
     </div>
   </div>
 </template>
