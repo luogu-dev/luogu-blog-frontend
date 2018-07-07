ca316eb4dabdd63fb3b44771e07c827326f746be
Himself65 <himself6565@gmail.com>
2018-07-08 05:30:45 +0800

    fixed bugs

--- src/templates/bread/common.js
+++ src/templates/bread/common.js
@@ -3,3 +3,10 @@ import 'bootstrap/dist/js/bootstrap.min'
 import 'font-awesome/css/font-awesome.min.css'
 import './styles/bread.css'
 import './styles/style.css'
+import { getPosts } from 'scripts/article_list'
+
+let searchBtn = document.querySelector('#search-btn')
+
+searchBtn.addEventListener('click', function () {
+  getPosts(1)
+})
--- src/templates/luogu3/common.js
+++ src/templates/luogu3/common.js
@@ -2,9 +2,10 @@ import 'amazeui/dist/css/amazeui.css'
 import 'amazeui'
 import './styles/style.css'
 import 'plugins/event_bus'
+/*
 import $ from 'jquery'
-import { getPosts } from 'scripts/article_list'
 
-$('#search-btn').addEventListener('click', function () {
-  getPosts(1)
+$('.main.menu').visibility({
+  type: 'fixed'
 })
+*/
