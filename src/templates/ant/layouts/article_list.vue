48a4f604c80be7314f7de42e67db2b7a305dcdbb
cleverdango <627677955@qq.com>
2019-01-23 17:50:29 +0800

    feature: 完善文章列表的排版和布局

--- src/templates/ant/layouts/article_list.vue
+++ src/templates/ant/layouts/article_list.vue
@@ -8,16 +8,53 @@
     >
       <p v-if="noTitleKey === 'article'">
         <a-list itemLayout="horizontal" :dataSource="listData">
-          <a-list-item slot="renderItem" slot-scope="item" class="card-container">
-            <a-list-item-meta
-              title="text 1"
-              description="段落示意：蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。"
-            ></a-list-item-meta>
-            <div class="test">
-              <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>
-              <a slot="title" href="https://vuecomponent.github.io/ant-design-vue/">{{item.title}}<span>某用户发布在httspans://ant.design</span></a>
-              
-            </div>
+          <a-list-item slot="renderItem" slot-scope="item">
+            <a-col>
+              <a-row>
+                <a-list-item-meta title="text 1"></a-list-item-meta>
+              </a-row>
+              <a-row>
+                <a-list-item-meta>
+                  <a-tooltip slot="description">
+                    <a-tag>test</a-tag>
+                    <a-tag>test2</a-tag>
+                  </a-tooltip>
+                </a-list-item-meta>
+              </a-row>
+              <a-row
+                class="article-content"
+              >段落示意：蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。</a-row>
+              <a-row>
+                <a href="">某用户</a>
+                <span class="inline-padding">发布了</span>
+                <a class="inline-padding-right" href="https://vuecomponent.github.io/ant-design-vue/">{{item.title}}</a>
+                <span class="text-grey">2019-01-23 02:44</span>
+              </a-row>
+              <a-row class="article-actions">
+                <span>
+                  <a-tooltip title="Like">
+                    <a-icon
+                      type="like"
+                      :theme="action === 'liked' ? 'filled' : 'outlined'"
+                      @click="like"
+                    />
+                  </a-tooltip>
+                  <span style="padding-left: '8px';cursor: 'auto'">111</span>
+                </span>
+                <a-divider type="vertical"/>
+                <span>
+                  <a-tooltip title="Message">
+                    <a-icon
+                      type="message"
+                      :theme="action === 'liked' ? 'filled' : 'outlined'"
+                      @click="message"
+                    />
+                  </a-tooltip>
+                  <span style="padding-left: '8px';cursor: 'auto'">111</span>
+                </span>
+
+              </a-row>
+            </a-col>
           </a-list-item>
         </a-list>
       </p>
@@ -28,7 +65,19 @@
 </template>
 
 <script>
-import { Card, List, ListItem, Avatar } from "ant-design-vue";
+import {
+  Card,
+  List,
+  ListItem,
+  Avatar,
+  contentList,
+  Tooltip,
+  Icon,
+  Col,
+  Row,
+  Divider,
+  Tag
+} from "ant-design-vue";
 import pagination from "../components/pagination.vue";
 import {
   defaultData,
@@ -71,7 +120,7 @@ export default {
         }
       ],
       key: "tab1",
-      noTitleKey: "app",
+      noTitleKey: "article",
       listData: [
         {
           title: "Ant Design Title 1"
@@ -105,7 +154,13 @@ export default {
     "a-list": List,
     "a-list-item": List.Item,
     "a-list-item-meta": List.Item.Meta,
-    "a-avatar": Avatar
+    "a-avatar": Avatar,
+    "a-tooltip": Tooltip,
+    "a-icon": Icon,
+    "a-row": Row,
+    "a-col": Col,
+    "a-divider": Divider,
+    "a-tag": Tag
   }
 };
 </script>
