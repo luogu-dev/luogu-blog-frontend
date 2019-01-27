a09992a951c89daa26710c224ca48256552bbc34
cleverdango <627677955@qq.com>
2019-01-28 01:18:21 +0800

    delete: 删除文章详情页不需要的组件

--- src/templates/ant/article.js
+++ src/templates/ant/article.js
@@ -2,18 +2,8 @@ import Vue from 'vue'
 import './common'
 import { initKatex, initHighlight } from 'scripts/article'
 import ArticleComments from './layouts/article_comments.vue'
-import ArticleVote from './layouts/article_vote.vue'
-import 'components/comment.css'
-import 'components/button.css'
-import 'components/form.css'
-import 'components/icon.css'
-import 'highlight.js/styles/tomorrow.css'
-import './styles/article.css'
 
-window.articleVote = new Vue({
-  el: '#article-vote',
-  render: h => h(ArticleVote)
-})
+import './styles/article.css'
 
 window.articleComments = new Vue({
   el: '#article-comments',
