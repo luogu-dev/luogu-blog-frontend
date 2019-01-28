522b9898a7939d9258871182a40788109e00c350
cleverdango <627677955@qq.com>
2019-01-29 00:00:28 +0800

    add: 完善点赞部分的逻辑

--- src/templates/ant/layouts/article_vote.vue
+++ src/templates/ant/layouts/article_vote.vue
@@ -1,41 +1,76 @@
 <template>
   <div id="article-vote">
-    <span :is="checkCanVote(-1) ? 'a' : 'span'"
-      href="javascript:void 0" @click="performVote(-1)"
+    <a-tooltip
+      title="踩"
+      :is="checkCanVote(-1) ? 'a' : 'span'"
+      href="javascript:void 0"
+      @click="dislike"
     >
-      <i class="icon thumbs down" :class="{ 'outline': vote !== -1 }"
-        style="transform: scale(-1,1);"
-      ></i>
-    </span>
-    赞：{{ thumbUp }}
-    <span :is="checkCanVote(1) ? 'a' : 'span'"
-      href="javascript:void 0" @click="performVote(1)"
+      <a-icon type="dislike" :theme="vote === '-1' ? 'filled' : 'outlined'"/>
+    </a-tooltip>
+    <a-divider type="vertical"/>
+
+    <a-tooltip
+      title="赞"
+      :is="checkCanVote(1) ? 'a' : 'span'"
+      href="javascript:void 0"
+      @click="like"
     >
-      <i class="icon thumbs up" :class="{ 'outline': vote !== 1 }"></i>
-    </span>
+      <a-icon type="like" :theme="vote === '1'? 'filled' : 'outlined'"/>
+      <span style="padding-left: '8px';cursor: 'auto'">{{ thumbUp }}</span>
+    </a-tooltip>
   </div>
 </template>
 
 <script>
-import { defaultData, performVote } from 'scripts/article'
+import { Icon, Divider, Tooltip } from "ant-design-vue";
+import { defaultData, performVote } from "scripts/article";
 export default {
-  data: defaultData,
+  data() {
+    return {
+      ...defaultData(),
+      action: null
+    };
+  },
   computed: {
-    canVoteUp () {
-      return this.checkCanVote(1)
+    canVoteUp() {
+      return this.checkCanVote(1);
     },
-    canVoteDown () {
-      return this.checkCanVote(-1)
+    canVoteDown() {
+      return this.checkCanVote(-1);
     }
   },
   methods: {
     performVote,
-    checkCanVote (voteType) {
+    checkCanVote(voteType) {
       // Must be logged in, and either not voted,
       // or voted the same voteType,
       // (in which case triggers un-vote)
-      return this.uid && (this.vote === 0 || this.vote === voteType)
+      return this.uid && (this.vote === 0 || this.vote === voteType);
+    },
+    like() {
+      // 如果没有表态，则赞
+      if (this.vote === '0') {
+        this.performVote(1);
+      } else if (this.vote === '1') {
+        // 如果已赞，则取消赞
+        this.performVote(0);
+      }
+    },
+    dislike() {
+      // 如果没有表态，则踩
+      if (this.vote === '0') {
+        this.performVote(-1);
+      } else if (this.vote === '-1') {
+        // 如果已踩，则取消踩
+        this.performVote(0);
+      }
     }
+  },
+  components: {
+    "a-icon": Icon,
+    "a-divider": Divider,
+    "a-tooltip": Tooltip
   }
-}
+};
 </script>
