8c88207c9ce0eb8cea1d5fdaa6597bfcae9ccf6c
Toto Lin <lin714093880@users.noreply.github.com>
2019-03-17 18:37:02 +0800

    Fix code highlight

--- src/templates/ant/article_list.js
+++ src/templates/ant/article_list.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import './common'
 import './styles/article_list.css'
 import ArticleList from './layouts/article_list.vue'
-
+import 'highlight.js/styles/tomorrow.css'
 import 'components/icon.css'
 
 window.articleList = new Vue({
