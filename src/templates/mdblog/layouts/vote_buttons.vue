acfebe4f94d6255ffd63990597f7cf68b6198c10
Henry-ZHR <1736466350@qq.com>
2020-08-11 22:10:38 +0800

    mdblog - 非作者不显示编辑按钮

--- src/templates/mdblog/layouts/vote_buttons.vue
+++ src/templates/mdblog/layouts/vote_buttons.vue
@@ -18,7 +18,7 @@
                       :hidden="vote != 0" v-if="uid"><i class="mdui-icon material-icons">thumb_down</i></button>
               <a class="mdblog-fab-icon mdui-fab mdui-fab-mini mdui-ripple mdui-color-orange" href="#blog-comments"> <i class="mdui-icon material-icons">chat</i></a>
               <a class="mdblog-fab-icon mdui-fab mdui-fab-mini mdui-ripple mdui-color-blue"
-                 :href="BlogGlobals.luoguAddress+'/blogAdmin/article/edit/' + BlogGlobals.blogID" v-if="BlogGlobals.isAuthor"><i class="mdui-icon material-icons">edit</i></a>
+                 :href="BlogGlobals.luoguAddress+'/blogAdmin/article/edit/' + BlogGlobals.blogID" v-if="BlogGlobals.isBlogAdmin"><i class="mdui-icon material-icons">edit</i></a>
           </div>
       </div>
 
