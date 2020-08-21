9132105bc58c646855b776f6bbfb9506692ca298
Soha Jin <soha@lohu.info>
2020-08-22 00:48:19 +0800

    fix highlight js

--- src/plugins/hljs.js
+++ src/plugins/hljs.js
@@ -1,29 +1,33 @@
-/* eslint-disable */
-import hljs from 'highlight.js/lib/highlight';
+import hljs from 'highlight.js/lib/core'
 import $ from 'jquery'
 
-let registeredLanguages = {};
+const registeredLanguages = {}
+const ensureLanguageRegistered = async lang => {
+  if (!registeredLanguages[lang]) {
+    try {
+      const { default: langMod } = await import(`highlight.js/lib/languages/${lang}`)
+      hljs.registerLanguage(lang, langMod)
+      registeredLanguages[lang] = true
+    } catch (e) {
+      console.error(`Failed to load language module: ${lang}`)
+      console.error(e)
+      throw e
+    }
+  }
+}
 
 window.hljs = hljs
 
 export { hljs }
-export async function renderAllCodeElements() {
+export async function renderAllCodeElements () {
+  await ensureLanguageRegistered('c-like')
   $('pre code').each(async (_, block) => {
-    let matchLang = $(block).attr('class').match(/language-(\w+)/)
-    if(!matchLang) {
+    const matchLang = $(block).attr('class').match(/language-(\w+)/)
+    if (!matchLang) {
       return
     }
-    let lang = matchLang[1]
-    if(!registeredLanguages.hasOwnProperty(lang)) {
-      try {
-        let langMod = await import(`highlight.js/lib/languages/${lang}`)
-        hljs.registerLanguage(lang, langMod.default)
-        registeredLanguages[lang] = true
-      } catch(e) {
-        console.error(`Failed to load language module: ${lang}`);
-        console.error(e);
-      }
-    }
+    const lang = matchLang[1]
+    await ensureLanguageRegistered(lang)
     hljs.highlightBlock(block)
   })
 }
